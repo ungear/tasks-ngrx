9fada2cc5f8839a65e6e94d1f2409cb7be2d176b
Mikhail Istomin <redeemer@inbox.ru>
2018-10-30 00:03:26 +0300

    simple counter example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { INCREMENT, DECREMENT, RESET } from "./store/counter";
+
+interface AppState {
+  count: number;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'tasks-ngrx';
+  count$: Observable<number>;
+
+  constructor(private store: Store<AppState>) {
+    this.count$ = store.pipe(select("count"));
+  }
+
+  increment() {
+    this.store.dispatch({ type: INCREMENT });
+  }
+
+  decrement() {
+    this.store.dispatch({ type: DECREMENT });
+  }
+
+  reset() {
+    this.store.dispatch({ type: RESET });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FiltersComponent } from './filters/filters.component';
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { FiltersComponent } from "./filters/filters.component";
+
+import { StoreModule } from "@ngrx/store";
+import { counterReducer } from "./store/counter";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    FiltersComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DashboardComponent, FiltersComponent],
+  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
